3cd2a9624e98950ade4e5377c2629461a5530d59
Brandon Reyes <101955462+ReyesKor07@users.noreply.github.com>
2024-08-31 01:03:16 -0500

    Folder BD Con

---
+++ Point-of-Sale/Logica/Conexion.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+
+namespace Point_of_Sale.Logica
+{
+    public class Conexion
+    {
+        private string BaseDatos;
+        private static Conexion Con = null;
+        
+        private Conexion() 
+        {
+            this.BaseDatos = "./BD_pointofsale.db";
+        }
+
+        public SQLiteConnection CrearConexion()
+        {
+            SQLiteConnection Cadena = new SQLiteConnection();
+            try
+            {
+                Cadena.ConnectionString = "Data Source=" + this.BaseDatos;
+            }
+            catch(Exception ex)
+            {
+                Cadena = null;
+                throw ex;
+            }
+            return Cadena;
+        }
+
+        public static Conexion getInstancia()
+        {
+            if( Con == null )
+            {
+                Con = new Conexion();
+            }
+            return Con;
+        }
+    }
+}
